84be8d6595755354213c4d299848650e73c851eb
Junwei Chen <kngzhi@hotmail.com>
2022-07-07 11:31:38 +0800

    feat: double arrow to down arrow

--- components/ArrowButton.tsx
+++ components/ArrowButton.tsx
@@ -1,32 +1,22 @@
 import styled from 'styled-components'
-import Image from 'next/image'
-import Arrows from '../public/images/utils/arrows.svg'
-const Button = styled.button`
+import { ArrowDown } from 'react-feather'
+const ArrowContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 
-  padding: 10px;
+  width: 32px;
+  height: 32px;
 
   background: #000000;
   border: 1px solid #000000;
-  border-radius: 20px;
+  border-radius: 50%;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.9);
-
-  &:hover {
-    border: 1px solid rgba(130, 130, 149, 0.3);
-    background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
-      linear-gradient(311.52deg, #3d464c -36.37%, #131619 62.81%);
-  }
-
-  &:active {
-    box-shadow: inset -1px -1px 1px rgba(255, 255, 255, 0.15);
-  }
 `
 export default function ArrowButton() {
   return (
-    <Button>
-      <Image src={Arrows} alt="trade-arrows" priority={true} />
-    </Button>
+    <ArrowContainer>
+      <ArrowDown color='#6F6E95' size={16} />
+    </ArrowContainer>
   )
 }
